868aa94ade4b22df6909a9b7bcc9e05c028b660b
Photis Theodorou <photisgtheodorou@gmail.com>
2023-05-15 12:44:09 -0400

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html', {})
+
+
+def blog(request):
+    return render(request, 'blog.html', {})
+
+
+def contact(request):
+    return render(request, 'contact.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {})
+
+
+def services(request):
+    return render(request, 'services.html', {})
