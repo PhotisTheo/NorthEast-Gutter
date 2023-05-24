3728741ef2f5d245e1a48756fc31239e83a5a9bc
Photis Theodorou <photisgtheodorou@gmail.com>
2023-05-24 14:34:12 -0400

    postgres

--- blog/settings.py
+++ blog/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'RzVpfsJzxSdk0o8jyCKw',
+        'HOST': 'containers-us-west-122.railway.app',
+        'PORT': '6174',
     }
 }
 
