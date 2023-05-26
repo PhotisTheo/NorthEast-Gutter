683b378b64ac59ad1be9a8c796348159e86b4d13
Photis Theodorou <photisgtheodorou@gmail.com>
2023-05-26 11:03:19 -0400

    updated csrf trusted origins

--- blog/settings.py
+++ blog/settings.py
@@ -20,7 +20,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://web-production-6a66.up.railway.app', 'https://*.127.0.0.1']
+    'northeast-gutter-production.up.railway.app', 'https://*.127.0.0.1']
 
 
 # Application definition
